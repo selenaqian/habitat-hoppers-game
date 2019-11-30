cfe27f15e9f53fb44b58c2a33a657b3c322cfd00
selenaqian <selenaq2010@gmail.com>
2019-11-29 23:23:31 -0500

    started single camera motion

---
+++ VMS326-finalproject/Assets/scripts/cameramovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameramovement : MonoBehaviour
+{
+    public Transform player1;
+    public Transform player2;
+    
+    public float xbound = 5.0f;
+    public float ybound = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void LateUpdate()
+    {
+        Vector3 delta1 = Vector3.zero;
+        
+        float dx1 = player1.position.x - transform.position.x; //x-distance between camera and player1
+        float dx2 = player2.position.x - transform.position.x; //x-distance between camera and player2
+    }
+}
