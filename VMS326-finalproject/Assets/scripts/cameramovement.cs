8c6978af90eb6b044b78e1156dfe7dc14f7f697b
selenaqian <selenaq2010@gmail.com>
2019-11-30 00:08:12 -0500

    basic movement x-direction

--- VMS326-finalproject/Assets/scripts/cameramovement.cs
+++ VMS326-finalproject/Assets/scripts/cameramovement.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class cameramovement : MonoBehaviour
 {
+    //set as the two players
     public Transform player1;
     public Transform player2;
     
-    public float xbound = 5.0f;
+    //sets when to move camera
+    public float xbound = 3.0f;
     public float ybound = 5.0f;
+    
+    //position and speed for lerp
+    Vector3 moveTo;
+    public float speed = 0.2f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -27,5 +34,23 @@ public class cameramovement : MonoBehaviour
         
         float dx1 = player1.position.x - transform.position.x; //x-distance between camera and player1
         float dx2 = player2.position.x - transform.position.x; //x-distance between camera and player2
+        
+        float dy1 = player1.position.y - transform.position.y; //y-distance between camera and player1
+        float dy2 = player2.position.y - transform.position.y; //y-distance between camera and player2
+        
+        if (dx1 > xbound)
+        {
+            delta1.x = dx1 - xbound;
+        }
+        //if x-distance too large on the right - dx > xbound - then move camera
+        //check if other player is still on screen - if not, drag them over also
+        
+        //if y-distsance too large going up - dy > ybound - then move camera
+        //check if other player still on screen - if not, they die
+        
+        //update moveTo position
+        moveTo = transform.position + delta1;
+        //lerp
+        transform.position = Vector3.Lerp(transform.position, moveTo, speed);
     }
 }
