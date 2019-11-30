0fc732ad08bdcd7ec22cf97b679ea5e61ac5360a
selenaqian <selenaq2010@gmail.com>
2019-11-30 01:13:48 -0500

    camera movement x-direction player1

--- VMS326-finalproject/Assets/scripts/cameramovement.cs
+++ VMS326-finalproject/Assets/scripts/cameramovement.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class cameramovement : MonoBehaviour
 {
+    //Goal 1: move camera to be ahead of the player when one of the players gets about 3/4 of the way to the right of the screen
+    
     //set as the two players
     public Transform player1;
     public Transform player2;
     
     //sets when to move camera
-    public float xbound = 3.0f;
-    public float ybound = 5.0f;
+    public float xboundright = 2.0f;
+    public float ybound = 0.05f;
+    public float xboundleft = 3.0f;
+    
+    //sets how far to move camera --> lower = move further
+    public float xmotion = 1.0f;
     
     //position and speed for lerp
     Vector3 moveTo;
     public float speed = 0.2f;
+    
+    Vector3 temp = Vector3.zero;
     
     // Start is called before the first frame update
     void Start()
@@ -32,24 +40,30 @@ public class cameramovement : MonoBehaviour
     {
         Vector3 delta1 = Vector3.zero;
         
-        float dx1 = player1.position.x - transform.position.x; //x-distance between camera and player1
-        float dx2 = player2.position.x - transform.position.x; //x-distance between camera and player2
+        float dx1 = player1.position.x - transform.position.x; //x-distance between player1 and camera
+        float dx2 = player2.position.x - transform.position.x; //x-distance between player2 and camera
         
-        float dy1 = player1.position.y - transform.position.y; //y-distance between camera and player1
-        float dy2 = player2.position.y - transform.position.y; //y-distance between camera and player2
+        float dy1 = player1.position.y - transform.position.y; //y-distance between player1 and camera
+        float dy2 = player2.position.y - transform.position.y; //y-distance between player2 and camera
         
-        if (dx1 > xbound)
-        {
-            delta1.x = dx1 - xbound;
-        }
         //if x-distance too large on the right - dx > xbound - then move camera
+        if (dx1 > xboundright)
+        {
+            //Debug.Log(dx1);
+            temp.x = dx1 - xmotion;
+        }
+        //if camera already in place where can see a little ahead in map, reset temp to 0
+        if (dx1 < -xboundleft)
+        {
+            //Debug.Log("stopping");
+            temp.x = 0;
+        }
         //check if other player is still on screen - if not, drag them over also
         
-        //if y-distsance too large going up - dy > ybound - then move camera
-        //check if other player still on screen - if not, they die
+        //if y-distance too large going up - dy > ybound - then zoom out if other one is too low, move if both going up
         
         //update moveTo position
-        moveTo = transform.position + delta1;
+        moveTo = transform.position + temp;
         //lerp
         transform.position = Vector3.Lerp(transform.position, moveTo, speed);
     }
