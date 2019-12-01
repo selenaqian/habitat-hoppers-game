97858861929b9efc03b5d294ca51715e70245b01
selenaqian <selenaq2010@gmail.com>
2019-11-30 19:11:40 -0500

    fixed zoom in

--- VMS326-finalproject/Assets/scripts/cameramovement.cs
+++ VMS326-finalproject/Assets/scripts/cameramovement.cs
@@ -34,6 +34,7 @@ public class cameramovement : MonoBehaviour
     private float newCamSize = 5.0f; //use for zoom in/out lerp
     private float origCamSize = 5.0f;
     public float x = 1.0f;
+    private float startZoomIn;
     
     // Start is called before the first frame update
     void Start()
@@ -49,6 +50,7 @@ public class cameramovement : MonoBehaviour
     
     private void LateUpdate()
     {
+        origCamSize = Camera.main.orthographicSize;
         Vector3 delta1 = Vector3.zero;
         
         float dx1 = player1.position.x - transform.position.x; //x-distance between player1 and camera
@@ -91,39 +93,71 @@ public class cameramovement : MonoBehaviour
         }
         
         //if y-distance too large going up - dy > ybound - then zoom out if other one is too low, move if both going up
-        //set up so move only for player1, zoom for player2
-        if (dy1 > yboundtop)
+        //set up so move based on midpoint of players, zoom based on individual on-screen
+        float dyplayers = player1.position.y - player2.position.y; //y-distance between the players
+        float ymidplayers = player2.position.y + dyplayers * 0.5f; //midpoint of the y-dist between the players
+        float dyoverall = ymidplayers - transform.position.y; //difference between midpoint of y-dist between players and camera
+        if (dyoverall > yboundtop)
         {
-            temp.y = dy1 - ymotion;
+            Debug.Log("moving up");
+            temp.y = dyoverall - ymotion;
             p1y = 1;
         }
-        else if (dy1 < -yboundtop)
+        else if (dyoverall < -yboundtop)
         {
-            temp.y = dy1 + ymotion;
+            Debug.Log("moving down");
+            temp.y = dyoverall + ymotion;
             p1y = 1;
         }
-        if (dy1 < yboundbottom && dy1 > -yboundbottom && p1y == 1)
+        if (dyoverall < yboundbottom && dyoverall > -yboundbottom && p1y == 1)
         {
             temp.y = 0;
             p1y = 0;
         }
         //zooming
-        if (dy2 > Camera.main.orthographicSize || dy2 < -Camera.main.orthographicSize) //if p2 offscreen in y-direction
+        if (dy1 > Camera.main.orthographicSize || dy1 < -Camera.main.orthographicSize) //if p1 off-screen
         {
-            //compute new orthographic size
+            Debug.Log("zoom out 1");
+            newCamSize = Mathf.Abs(dy1) + 0.1f;
+            p2y = 1;
+        }
+        else if (dy2 > Camera.main.orthographicSize || dy2 < -Camera.main.orthographicSize) //if p2 off-screen
+        {
+            Debug.Log("zoom out 2");
             newCamSize = Mathf.Abs(dy2) + 0.1f;
             p2y = 1;
         }
-        if (dy2 < 2.5f && dy2 > - 2.5f && p2y == 1) //if p2 in middle of screen in y-direction
+        else if (dyplayers < 5.0f && p2y == 1 && p1y == 0) //if both players on-screen in default range
+        {
+            Debug.Log("zoom in");
+            newCamSize = 5.0f;
+            p2y = 0;
+            startZoomIn = Time.time;
+        }
+        /*if (dyplayers > Camera.main.orthographicSize) //if either player offscreen in y-direction
+        {
+            //compute new orthographic size
+            newCamSize = dyplayers + 0.1f;
+            p2y = 1;
+        }
+        if (dyplayers < 5.0f) //if p2 in middle of screen in y-direction
         {
             newCamSize = 5.0f;
-        }
+        }*/
         
         //update moveTo position
         moveTo = transform.position + temp;
         //lerp
         transform.position = Vector3.Lerp(transform.position, moveTo, speed);
-        Camera.main.orthographicSize = Mathf.Lerp(origCamSize, newCamSize, x);
+        //update camera size
+        if ((Time.time - startZoomIn)*0.3f < 1.0f)
+        {
+            Camera.main.orthographicSize = Mathf.Lerp(origCamSize, newCamSize, (Time.time - startZoomIn)*0.3f);
+        }
+        else
+        {
+            Camera.main.orthographicSize = Mathf.Lerp(origCamSize, newCamSize, x);
+        }
         
         //check if players are still on screen - if not, drag them over also
         if (dx2 < - (float)Screen.width/(float)Screen.height * Camera.main.orthographicSize)
