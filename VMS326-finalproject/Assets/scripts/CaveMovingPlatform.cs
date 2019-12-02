b81ebed4264eb3eacf57d9d1aee357c43f06339c
selenaqian <selenaq2010@gmail.com>
2019-12-01 22:27:48 -0500

    finished cave platforms, added moving platform

---
+++ VMS326-finalproject/Assets/scripts/CaveMovingPlatform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveMovingPlatform : MonoBehaviour
+{
+    public AnimationCurve myCurve;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, myCurve.Evaluate((Time.time % myCurve.length)), transform.position.z);
+    }
+}
