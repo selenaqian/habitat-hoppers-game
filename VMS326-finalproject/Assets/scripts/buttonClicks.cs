f372e65504ea191010fa4e28230374502199a114
selenaqian <selenaq2010@gmail.com>
2019-12-02 23:30:36 -0500

    main menu buttons work

---
+++ VMS326-finalproject/Assets/scripts/buttonClicks.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonClicks : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void StartClick()
+    {
+        //Debug.Log("start click");
+        SceneManager.LoadScene("Introduction");
+    }
+    
+    public void InstructionsClick()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+}
